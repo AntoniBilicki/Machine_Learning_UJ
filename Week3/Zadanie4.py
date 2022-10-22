0596f1f255851888f2e074df9ab42328d400de67
AntoniB <antoni.bilicki@gmail.com>
2022-10-22 18:57:58 +0200

    Finally finished task 4

--- Week3/Zadanie4.py
+++ Week3/Zadanie4.py
@@ -4,6 +4,7 @@ import torch
 from utils import visualize_optimizer
 from checker import test_optimizer
 from Zadanie3 import Optimizer
+import numpy as np
 from typing import List
 
 class Adagrad(Optimizer):
@@ -12,10 +13,23 @@ class Adagrad(Optimizer):
         super().__init__(initial_params)
         self.learning_rate = learning_rate
         self.epsilon = epsilon
-        self.gt = 0
+        self.gt = torch.zeros_like(self.params[0])
+        self.gradMemory = []
 
-        @torch.no_grad()
-        def step(self):
-            for idx, param in enumerate(self.params):
-            pass
+        for param in self.params:
+            self.gradMemory.append(torch.zeros_like(param))
+
+    @torch.no_grad()
+    def step(self):
+        for param in self.params:
+            self.gradMemory.append(torch.pow(param.grad, 2))
+            self.gt = torch.sum(torch.stack(self.gradMemory), dim=0)
+            param -= (self.learning_rate/(self.gt + self.epsilon)**0.5) * param.grad
+
+
+
+
+#test_optimizer(Adagrad)
+visualize_optimizer(Adagrad, n_steps=20, learning_rate=1.0, epsilon=1e-8)
+plt.show()
 
