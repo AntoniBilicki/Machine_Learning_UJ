b5ace0609ae774e5e24be0615cc4fd59bd078f9b
AntoniB <antoni.bilicki@gmail.com>
2022-10-22 22:59:47 +0200

    Task 6 done

--- Week3/Zadanie6.py
+++ Week3/Zadanie6.py
@@ -8,7 +8,7 @@ import numpy as np
 from typing import List
 
 
-class RMSProp(Optimizer):
+class Adadelta(Optimizer):
 
     def __init__(self, initial_params, gamma, epsilon):
         super().__init__(initial_params)
@@ -17,20 +17,16 @@ class RMSProp(Optimizer):
         self.ht = torch.zeros_like(self.params[0])
         self.dt = torch.zeros_like(self.params[0])
 
-        self.gradMemory = []
-
-        for param in self.params:
-            self.gradMemory.append(torch.zeros_like(param))
-
     @torch.no_grad()
     def step(self):
         for param in self.params:
-            self.gradMemory.append(torch.pow(param.grad, 2))
             self.ht = self.gamma*self.ht + (1-self.gamma)*torch.pow(param.grad, 2)
-            param -= (self.learning_rate / (self.ht + self.epsilon) ** 0.5) * param.grad
+            term = ((self.dt + self.epsilon)**0.5 / (self.ht + self.epsilon) ** 0.5) * param.grad
+            param -= term
+            self.dt = self.gamma*self.dt + (1-self.gamma)*term**2
 
 
 
-visualize_optimizer(Adagrad, n_steps=20, learning_rate=1.0, epsilon=1e-8)
+visualize_optimizer(Adadelta, n_steps=20, epsilon=5e-2, gamma=0.9)
 plt.show()
 
